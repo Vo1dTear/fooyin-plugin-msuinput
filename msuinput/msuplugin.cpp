c98d1a90ae45669603075bee20240f518396b42d
Vo1dTear <vo1dtear.01@gmail.com>
2026-05-06 03:21:07 -0400

    fix: migrate msuinput to new settings interface

--- msuinput/msuplugin.cpp
+++ msuinput/msuplugin.cpp
@@ -2,48 +2,56 @@
 #include "msusettingswidget.h"
 #include "msuinput.h"
 
-namespace Fooyin::MSU {
+#include <gui/plugins/pluginsettingsprovider.h>
 
+namespace Fooyin::MSU {
+    
+    namespace {
+        
+        class MSUSettingsProvider final : public PluginSettingsProvider
+        {
+        public:
+            void showSettings(QWidget* parent) override
+            {
+                auto* dialog = new MSUSettingsWidget(parent);
+                dialog->setAttribute(Qt::WA_DeleteOnClose);
+                dialog->show();
+            }
+        };
+        
+    } // namespace
+    
     QString MSUPlugin::inputName() const
     {
         return QStringLiteral("MSU-1");
     }
-
+    
     InputCreator MSUPlugin::inputCreator() const
     {
         InputCreator creator;
         creator.decoder = []() {
             auto decoder = std::make_unique<MSUDecoder>();
-
+            
             QSettings settings;
             double gainDb = settings.value("MSU/Gain", 0.0).toDouble();
             quint32 loopCount = settings.value("MSU/LoopCount", 0).toUInt();
             bool enableLoop = settings.value("MSU/EnableLoop", true).toBool();
-
+            
             decoder->setGain(gainDb);
             decoder->setLoopCount(loopCount);
             decoder->setEnableLoop(enableLoop);
-
+            
             return decoder;
         };
-        creator.reader  = []() { return std::make_unique<MSUReader>(); };
+        creator.reader = []() { return std::make_unique<MSUReader>(); };
         return creator;
     }
-
-    // Inform the framework that this plugin has settings
-    bool MSUPlugin::hasSettings() const
+    
+    std::unique_ptr<PluginSettingsProvider> MSUPlugin::settingsProvider() const
     {
-        return true;
+        return std::make_unique<MSUSettingsProvider>();
     }
-
-    // Show the settings window consistently with other plugins
-    void MSUPlugin::showSettings(QWidget* parent)
-    {
-        auto* dialog = new MSUSettingsWidget(parent);
-        dialog->setAttribute(Qt::WA_DeleteOnClose); // Qt will delete the dialog on close
-        dialog->show(); // non-blocking
-    }
-
+    
 } // namespace Fooyin::MSU
 
 #include "moc_msuplugin.cpp"
