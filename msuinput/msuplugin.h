9ab64e23aa5e51402d790587e8905ac44ea7d42e
Vo1dTear <vo1dtear.01@gmail.com>
2026-05-07 04:53:18 -0400

    Update plugin metadata IID

--- msuinput/msuplugin.h
+++ msuinput/msuplugin.h
@@ -10,12 +10,12 @@ namespace Fooyin::MSU {
     class MSUPlugin : public QObject, public Plugin, public InputPlugin
     {
         Q_OBJECT
-        Q_PLUGIN_METADATA(IID "org.fooyin.fooyin.plugin" FILE "msuinput.json")
+        Q_PLUGIN_METADATA(IID "org.fooyin.fooyin.plugin/1.0" FILE "msuinput.json")
         Q_INTERFACES(Fooyin::Plugin Fooyin::InputPlugin)
-
+        
     public:
         [[nodiscard]] QString inputName() const override;
         [[nodiscard]] InputCreator inputCreator() const override;
     };
-
+    
 } // namespace Fooyin::MSU
